41ff45b6d6edb1e828733abf2e0cf465936c942d
juannmmaa <juanma1992_20@hotmail.com>
2013-11-15 01:11:35 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE HTML>
+
+
+<html>  
+    <head>
+    <title> Ingenieria de software </title>
+    <meta name="description" content="Weblog de juanma"/>
+    <meta name="keywords" content="weblog, blog, juan marcelo cortez gutierrez, juanma" />
+    <meta http-equiv="refresh" content="500"/>
+    <link rel=stylesheet href="estilo.css" style="text/css" />
+    </head>    
+
+    <body link="black">
+    
+    <div id="principal">
+    
+            <header>
+            <div id="logo">    <svg
+   xmlns:dc="http://purl.org/dc/elements/1.1/"
+   xmlns:cc="http://creativecommons.org/ns#"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:xlink="http://www.w3.org/1999/xlink"
+   version="1.1"
+   width="40mm"
+   height="30mm"
+   id="svg2"><a href="index.html">Inicio </a>
+  <defs
+     id="defs4" />
+  <metadata
+     id="metadata7">
+    <rdf:RDF>
+      <cc:Work
+         rdf:about="">
+        <dc:format>image/svg+xml</dc:format>
+        <dc:type
+           rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+        <dc:title></dc:title>
+      </cc:Work>
+    </rdf:RDF>
+  </metadata>
+  <g
+     id="layer1">
+    <image
+       xlink:href="file:///C:/Users/juanma/blog/img/logo2.png"
+       x="50"
+       y="50"
+       width="60"
+       height="60"
+       id="image2993" />
+  </g>
+</svg>
+</div>
+            <h1> <font size="8"> INGENIERIA DE SOFTWARE </font> </h1>
+			<br></br>
+            <h2> Grupo 05: Revista Digital </h2>
+             <nav>
+                <ul>
+                    <li><a href="index.php"> <canvas id="Inicio" width="50" height="50" > <br></canvas></br> Inicio</a> </li>
+                   
+         
+                 </ul>
+             </nav>
+            </header>
+
+            <div id="contenido">
+                <section id="sobremi">
+				
+				
+				<div id="frases"> 
+				<br></br>
+				<font color="white" size="6"> 
+<?php
+// Completamos el vector con frases
+$vector = array(
+1 => "Preguntarse cu&aacutendo los ordenadores podr&aacuten pensar es como preguntarse cu&aacutendo los submarinos podr&aacuten nadar
+-- Edsger W. Dijkstra",
+2 => "La funci&oacuten de un buen software es hacer que lo complejo aparente ser simple
+-- Grady Booch",
+
+);
+
+// Obtenemos un número aleatorio
+$numero = rand(1,2);
+
+// Imprimimos la frase
+echo "$vector[$numero]";
+?>
+			</font>	
+				</div>
+               
+                </section>
+            
+            <section>
+                <article>
+				
+                <div id="pelao"> </div>
+				<font color="blue" size="4"> 
+                 <time> 07-06-2013 </time>
+                      <h3> Felipe Eduardo Alvarez Rojas </h3>
+                    <h4> Alumno de Ingenieria Civil en Computacion</h4>
+                    <p>   egresado el año 1965 del Liceo Jose Victorino Lastarria a la edad de 39 años. Muchos años pasaron
+						hasta que logro entrar a la universidad. El estudio ha sido su fuente de salvacion ya que a mediados de los
+						setenta cayo en las drogas, pero estar encerrado bajo el rigor de la ley lo ayudo a rehabilitarse y conocer
+						lo duro de la prision mediante abusos sexuales. Luego de quedar en libertad se encontraba solo por lo que busco refugio
+						en la comida y el alcohol, convirtiendose en el ser humano que conocemos ahora.</p>
+						<p> Enlaces de Interes: </p>
+						<p> <A HREF="https://github.com/fliseven/"> Repositorio Github</A></p>
+						<p> <A HREF="https://www.facebook.com/falvarezrojas1?fref=ts"> Facebook</A></p>
+						<p>  Correo: fli7@hotmail.com</p>
+						
+                   
+                </article>
+				
+				<article>
+                <div id="khryz"> </div>
+				<font color="blue" size="4"> 
+                 
+                      <h3> Christopher Salvatierra Lagos </h3>
+                    <h4> Alumno de Ingenieria Civil en Computacion</h4>
+                    <p> 
+                        Tengo 22 años, soy alumno regular de Ingenieria Civil en Computacion de ingreso en el 2010. En el 2009 egrese de enseñanza media del Liceo de Aplicacion.
+                        Actualmente curso el octavo semestre de la carrera que estudio.
+                        Dentro de mis hobbies al que mas le dedico tiempo es al Running
+                    </p>
+                    <p> <a href = "https://github.com/Chris2141"> Repositorio Github </a> </p>
+                    <p> <a href="https://www.facebook.com/chris2141"> Facebook </a></p>
+                    <p> Correo: c.salvatierra.icci@gmail.com </p>
+
+                   
+                </article>
+                      
+                <article>
+                <div id="juanma"> </div>
+				<font color="blue" size="4"> 
+                 <time> 07-06-2013 </time>
+                      <h3> Juan Marcelo Cortez Gutierrez </h3>
+                    <h4> Alumno de Ingenieria Civil en Computacion</h4>
+                    <p> Andres Calamardo! Andres Calamardo! Andres Calamardo! Andres Calamardo! Andres Calamardo! Andres Calamardo! Andres Calamardo! Andres Calamardo! Andres Calamardo! Andres Calamardo! Andres Calamardo! Andres Calamardo! Andres Calamardo! Andres Calamardo! Andres Calamardo! Andres Calamardo! Andres Calamardo! Andres Calamardo! Andres Calamardo! Andres Calamardo! Andres Calamardo! Andres Calamardo! Andres Calamardo! Andres Calamardo! Andres Calamardo! Andres Calamardo! Andres Calamardo! Andres Calamardo!  </p>
+                    <audio controls="controls">
+                    <source src="audio/calamaro.ogg" type="audio/ogg"/>
+
+                   
+                </article>
+             </section>
+
+             </div>
+               
+             <footer>
+             <h6> 
+                <div class="vcard">
+                        <div> <span class="name"> Juan Marcelo Cortez Gutierrez</span> - <span class="locality"> Santiago </span> - <span class="region">Region Metropolitana </span> - <span class="postal-code">+56</span> </div>
+                        <div> <span class="tel"> 96 000 00 00 </span> - <span class="email"> juanma.cortezg@gmail.com</span> </div>
+                </div>
+              </h6>
+              <script type="text/javascript">
+              if(localStorage.pagecount){localStorage.pagecount=Number(localStorage.pagecount)+1;} else {localStorage.pagecount=1;}
+              document.write("Has visitado mi pagina " + localStorage.pagecount + " veces");
+              </script>
+
+              <script type="text/javascript">
+              if(sessionStorage.pagecount){sessionStorage.pagecount= Number(sessionStorage.pagecount)+1;} else{sessionStorage.pagecount=1;}
+              document.write("en esta sesion, has visitado mi pagina: " + sessionStorage.pagecount + " veces");
+              </script>
+             </footer>
+         </div>
+    </body>
+    
+</html>
+
+
+
